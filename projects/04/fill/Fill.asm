b5e869808c08e719d80c2918951caa77c6935ca6
Ian Dominey <ian.dominey@gmail.com>
2022-02-17 20:17:24 +0000

    Created screen fill program.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,41 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(CHK_KBD)
+@SCREEN
+D=A
+@CURRENT_SCREEN
+M=D
+@KBD
+D=M
+@CLEAR_SCREEN
+D;JEQ
+@FILL_SCREEN
+0;JMP
+
+(CLEAR_SCREEN)
+@R0
+M=0
+@UPDATE_SCREEN
+0;JMP
+
+(FILL_SCREEN)
+@R0
+M=-1
+
+(UPDATE_SCREEN)
+@R0
+D=M
+@CURRENT_SCREEN
+A=M
+M=D
+@CURRENT_SCREEN
+MD=M+1
+@SCREEN
+D=D-A
+@8192
+D=A-D
+@UPDATE_SCREEN
+D;JGT
+@CHK_KBD
+0;JMP
